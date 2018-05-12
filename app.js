0300ac9f6b1fdd923325127dae43c2178aed707a
tommy-tran <trant28@mcmaster.ca>
2018-05-12 04:34:28 -0400

    generateKeys method for private and public keys completed

--- app.js
+++ app.js
@@ -17,9 +17,7 @@ app.get('/', (req, res, next) => {
   res.send('root');
 });
 
-app.post('/create', (req, res) => {
-
-});
+app.post('/create', (req, res) => {});
 
 app.use('/lb/:key', requestRoute);
 
@@ -28,3 +26,31 @@ app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
+const generateKeys = () => {
+  const privateKey = Math.random()
+    .toString(36)
+    .replace('0.', '');
+  const publicKey = Math.random()
+    .toString(36)
+    .replace('0.', '');
+
+  // To handle unlikely case of both private and public keys being the same
+  if (privateKey === publicKey) {
+    return generateKeys();
+  }
+
+  // Find if the generated keys currently exist in the databse
+  leaderboard
+    .find({
+      $or: [{ privateKey }, { publicKey }]
+    })
+    .then(() => {
+      // Found leaderboard with generated key
+      return generateKeys();
+    });
+
+  return {
+    privateKey,
+    publicKey
+  };
+};
