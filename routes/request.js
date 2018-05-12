e6ac3cf996f27c57d8562c0713f3a3634cfb59c4
tommy-tran <trant28@mcmaster.ca>
2018-05-12 03:32:11 -0400

    Planning routes

--- routes/request.js
+++ routes/request.js
@@ -4,38 +4,64 @@ const leaderboard = mongoose.model('leaderboards');
 const router = express.Router({ mergeParams: true });
 
 router.get('/*', (req, res, next) => {
-  // Pass any match to private or public key
   const request = getRequest(req.key);
 
   if (request) {
-    req.body = request;
+    req.requestData = request;
     next();
   } else {
     res.end();
   }
 });
 
-router.get('/:user', (req, res) => {
-  res.send(req.body);
+
+/**
+ * Public routes
+ */
+router.get('/user/:user', (req, res) => {
+  // Return user data
 });
+
+router.delete('/:user', (req, res) => {
+  // Check if private key and delete user score from leaderboard
+});
+
+router.post('/user/:user/:points', (req, res) => {
+  // Check if private key and set new score/time
+});
+
+/**
+ * Private routes
+ */
+router.get('/publickey', (req, res) => {
+  // Check if private key and return public key
+});
+
+router.get('/clear', (req, res) => {
+  // Check if private key and clear leaderboard
+});
+
+
+
 
 /**
  * Checks for request type and returns object specifying request type and the request itself
  */
 const getRequest = key => {
+  // Used until database is set up
   return { type: 'private', data: null };
-  
+
   const privateRequest = leaderboard.findOne({
     privateKey: key
+  }, result => {
+    return { type: 'private', data: privateRequest }
   });
-
-  if (privateRequest) return { type: 'private', data: privateRequest };
 
   const publicRequest = leaderboard.findOne({
     publicKey: key
+  }, result => {
+    return { type: 'public', data: publicRequest }
   });
-
-  if (publicRequest) return { type: 'public', data: publicRequest };
 
   return null;
 };
