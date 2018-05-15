c095bbbf04e1e93d388e9feb19f1141e403946d9
Ali Maqsood <ali-maqsood@Hotmail.com>
2018-05-15 13:48:25 -0400

    Update User.js
    Edited user Schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = {
+const userSchema = new Schema({
   username: {
     type: String,
     required: true
@@ -22,7 +22,7 @@ const userSchema = {
     type: String,
     required: () => this.score || this.time
   }
-};
+});
 
 const User = mongoose.model('User', userSchema);
 
