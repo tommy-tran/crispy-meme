2b2a81df5314fb32b9d788a169d4376a8d7c48a5
tommy-tran <trant28@mcmaster.ca>
2018-05-20 18:05:16 -0400

    Fix dashboard extending past window, private key shows on create

--- client/src/components/LeaderboardInfo/LeaderboardInfo.js
+++ client/src/components/LeaderboardInfo/LeaderboardInfo.js
@@ -5,12 +5,35 @@ import { Redirect } from 'react-router-dom';
 const LeaderboardInfo = props => {
   let redirect = null;
   let table = null;
+  let privateMsg = null;
 
   if (!props.leaderboard) {
     redirect = <Redirect to="/" />;
   } else {
-    const { gameName, ownerName, email, dateCreated, publicKey } = props.leaderboard;
-    console.log(props.leaderboard);
+    const {
+      gameName,
+      ownerName,
+      email,
+      dateCreated,
+      publicKey
+    } = props.leaderboard;
+
+    let privateRow = null;
+
+    if (props.leaderboard.privateKey) {
+      privateRow = (
+        <tr>
+          <td>Private Key</td>
+          <td>{props.leaderboard.privateKey}</td>
+        </tr>
+      );
+      privateMsg = (
+        <div style={{ textAlign: 'center', color: 'red' }}>
+          Please save the private key!
+        </div>
+      );
+    }
+
     table = (
       <table>
         <tbody>
@@ -30,6 +53,7 @@ const LeaderboardInfo = props => {
             <td>Public Key</td>
             <td>{publicKey}</td>
           </tr>
+          {privateRow}
           <tr>
             <td>Date Created</td>
             <td>{dateCreated.split('T')[0]}</td>
@@ -44,6 +68,7 @@ const LeaderboardInfo = props => {
       <div className="LeaderboardInfo__Block">CURRENT LEADERBOARD</div>
       {redirect}
       {table}
+      {privateMsg}
     </div>
   );
 };
