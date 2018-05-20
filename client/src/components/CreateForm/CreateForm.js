034c4811a2505142c76723e1c002245544e7b9be
tommy-tran <trant28@mcmaster.ca>
2018-05-20 15:51:43 -0400

    Styling mobile for create form

--- client/src/components/CreateForm/CreateForm.js
+++ client/src/components/CreateForm/CreateForm.js
@@ -1,1 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './CreateForm.css';
+import Input from '../UI/Input/Input';
+
+class CreateForm extends Component {
+  state = {
+    gameName: '',
+    ownerName: '',
+    email: ''
+  };
+
+  gameNameHandler = event => {
+    this.setState({
+      gameName: event.target.value
+    });
+  };
+
+  ownerNameHandler = event => {
+    this.setState({
+      ownerName: event.target.value
+    });
+  };
+
+  emailHandler = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="CreateForm">
+        <i className="far fa-trophy-alt fa-10x Logo" />
+        <Input inputType="gameName" changed={this.gameNameHandler} />
+        <Input inputType="ownerName" changed={this.ownerNameHandler} />
+        <Input inputType="email" changed={this.emailHandler} />
+        <div className="CreateForm__ButtonBox">
+          <i className="fal fa-check-circle fa-5x confirm" />
+          <i className="fal fa-times-circle fa-5x clear" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreateForm;
--- client/src/components/UI/Input/Input.js
+++ client/src/components/UI/Input/Input.js
@@ -1,28 +1,38 @@
 import React from 'react';
+import './Input.css';
 
-const Input = (props) => {
-  const input = <input name={props.inputType} type={props.inputType === 'email' ? 'email' : 'text'} onChange={props.changed} /> 
+const Input = props => {
+  const input = (
+    <input className="Input"
+      name={props.inputType}
+      type={props.inputType === 'email' ? 'email' : 'text'}
+      onChange={props.changed}
+    />
+  );
 
   let label = null;
 
-  switch(props.inputType) {
+  switch (props.inputType) {
     case 'ownerName':
-      label = 'Enter your name:';
+      label = 'Enter your name: ';
       break;
     case 'gameName':
-      label = "Enter the game or the competition's name";
+      label = "Enter the game/competition: ";
       break;
     case 'email':
-      label = 'Enter your email:'
+      label = 'Enter your email: ';
       break;
-    default: 
+    default:
   }
 
-  const fullInput = <label>{label}{input}</label>
+  const fullInput = (
+    <label className="Label">
+      {label}
+      {input}
+    </label>
+  );
 
-  return (
-    {fullInput}
-  )
-}
+  return fullInput;
+};
 
 export default Input;
