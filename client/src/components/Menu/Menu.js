d40ea6cb784cd6938026ee4a4b9142862b8074cc
tommy-tran <trant28@mcmaster.ca>
2018-05-22 11:02:49 -0400

    Menu properly routes when no current leaderboard

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 import Backdrop from '../UI/Backdrop/Backdrop';
 import './Menu.css';
@@ -7,18 +8,40 @@ import './Menu.css';
 class Menu extends Component {
   render() {
     let menuOptions = null;
-    if (this.props.leaderboard) {
+    if (!this.props.leaderboard) {
+      menuOptions = (
+        <div className="Menu__Options">
+          <NavLink className="Menu__Option" to="/" onClick={this.props.clicked}>
+            Input a leaderboard key
+          </NavLink>
+          <NavLink
+            className="Menu__Option"
+            to="/create"
+            onClick={this.props.clicked}
+          >
+            Create a new Leaderboard
+          </NavLink>
+          <NavLink
+            className="Menu__Option"
+            to="/about"
+            onClick={this.props.clicked}
+          >
+            About
+          </NavLink>
+        </div>
+      );
+    } else {
+      menuOptions = (
+        <div className="Menu__Options">
+          <div className="Menu__Option">Input another key</div>
+          <div className="Menu__Option">Create a new leaderboard</div>
+          <div className="Menu__Option">Current Leaderboard</div>
+          <div className="Menu__Option">Delete Leaderboard</div>
+          <div className="Menu__Option">Add User</div>
+          <div className="Menu__Option">About</div>
+        </div>
+      );
     }
-
-    menuOptions = (
-      <div className="Menu__Options">
-        <div className="Menu__Option">Input another key</div>
-        <div className="Menu__Option">Add User</div>
-        <div className="Menu__Option">Current Leaderboard</div>
-        <div className="Menu__Option">Delete Leaderboard</div>
-        <div className="Menu__Option">About</div>
-      </div>
-    );
 
     const display = this.props.show ? (
       <div className="MenuContainer">
